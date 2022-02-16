3ebdd292fe7a50b974bc7e43973df3f63eb6a0c9
unknown <david.aboagye@amalitech.org>
2022-02-16 12:34:41 +0000

    fetched and paginated repositories

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,68 +1,105 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useUserContext } from "../context/userContext";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import axios from "axios";
 import Pagination from "./Pagination";
 
-
 const Dashboard = () => {
-  const { logoutUser } = useUserContext();
-  const [username, setUsername] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [organizations, setOrganizations] = useState([])
-  
+   const [repositories, setRepositories] = useState([])
+  let { logoutUser } = useUserContext();
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [organizations, setOrganizations] = useState([]);
+
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(5);
+  const [postsPerPage] = useState(4);
 
+  const [currentRepositoryPage, setCurrentRepositoryPage] = useState(1);
+  const [postsPerRepositoryPage] = useState(3);
 
-
-  function handleSubmit(e){
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
     searchOrganizations();
   }
 
-  function searchOrganizations(){
+  function searchOrganizations() {
     setLoading(true);
-    axios({method : 'get',
-    url: `https://api.github.com/users/${username}/orgs`
-    }).then(res => {
-      setLoading(false)
+    axios({
+      method: "get",
+      url: `https://api.github.com/users/${username}/orgs`,
+    }).then((res) => {
+      setLoading(false);
       setOrganizations(res.data);
-      console.log(res.data)
-      console.log(res)
-    })
+      console.log(res.data);
+      console.log(res);
+    });
+    setRepositories([])
   }
-  function renderOrganization(organization){
+  function getRepos(event) {
+    let name = event.target.value;
+    console.log(name);
+    axios({
+      method: "get",
+      url: `https://api.github.com/users/${name}/repos`,
+    }).then((res) => {
+      setRepositories(res.data);
+      console.log(res.data);
+    });  
+  }
+  function renderRepositories(repositories){
     return(
-      <ul className='list-group mb-2' key={organization.id}>
-        <li  className='list-group-item'>
-          {organization.login}
+        <li name='repository-name' className='list-group-item' key={repositories.id}>
+          <input type='button' value={repositories.name}  className="organization-name" />
         </li>
-      </ul>
     )
+  }
+
+  function renderOrganization(organization) {
+    return (
+      <li
+        name="organization-name"
+        className="list-group-item"
+        key={organization.id}
+      >
+        <input
+          type="button"
+          value={organization.login}
+          onClick={getRepos}
+          className="organization-name"
+        />
+      </li>
+    );
   }
 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = organizations.slice(indexOfFirstPost, indexOfLastPost);
-  const paginate = pageNumber => setCurrentPage(pageNumber);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const indexOfLastRepositoryPost = currentRepositoryPage * postsPerRepositoryPage;
+  const indexOfFirstRepositoryPost = indexOfLastRepositoryPost - postsPerRepositoryPage;
+  const currentRepositoryPosts = repositories.slice(indexOfFirstRepositoryPost, indexOfLastRepositoryPost);
+  const paginateRepositories = (pageNumber) => setCurrentRepositoryPage(pageNumber);
 
   return (
     <div>
       <div className="">
-      <div className="">
-      <div className="container mt-5">
-      <input className="input"
-      value={username}
-      placeholder = 'Github Username'
-      onChange={e =>setUsername(e.target.value)} 
-      />
-      <button className="button" onClick={handleSubmit}>{loading? "Searching..." : "Search"} </button>
-      </div>
-      <div className="results-container">
-        {currentPosts.map(renderOrganization)}
-      </div>
-      </div>
+        <div className="container mt-5">
+          <input
+            className="input"
+            value={username}
+            placeholder="Github Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button className="button" onClick={handleSubmit}>
+            {loading ? "Searching..." : "Search"}
+          </button>
+        </div>
+        <div className="results-container">
+          <ul className="list-group mb-2">
+            {currentPosts.map(renderOrganization)}
+          </ul>
+        </div>
       </div>
 
       <Pagination
@@ -70,11 +107,21 @@ const Dashboard = () => {
         totalPosts={organizations.length}
         paginate={paginate}
       />
+       <div className="results-container">
+          <ul className="list-group mb-2">
+            {currentRepositoryPosts.map(renderRepositories)}
+          </ul>
+        </div>
+        <Pagination
+        postsPerPage={postsPerRepositoryPage}
+        totalPosts={repositories.length}
+        paginate={paginateRepositories}
+      />
 
       <Router>
-      <Link to="/auth">
-      <button onClick={logoutUser}>Log out</button>
-      </Link>
+        <Link to="/auth">
+          <button onClick={logoutUser}>Log out</button>
+        </Link>
       </Router>
     </div>
   );
